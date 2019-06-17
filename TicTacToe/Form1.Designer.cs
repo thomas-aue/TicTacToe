efdf933ce936c403b6b33b345d2a3a679e7dd0ea
Thomas Aue <51162572+thomas-aue@users.noreply.github.com>
2019-06-17 10:55:04 +0200

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripFile = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // b7
+            // 
+            this.b7.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b7.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b7.FlatAppearance.BorderSize = 0;
+            this.b7.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightGray;
+            this.b7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b7.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b7.Location = new System.Drawing.Point(13, 42);
+            this.b7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(100, 100);
+            this.b7.TabIndex = 0;
+            this.b7.UseVisualStyleBackColor = false;
+            this.b7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b8
+            // 
+            this.b8.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b8.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b8.FlatAppearance.BorderSize = 0;
+            this.b8.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b8.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b8.Location = new System.Drawing.Point(121, 42);
+            this.b8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(100, 100);
+            this.b8.TabIndex = 1;
+            this.b8.UseVisualStyleBackColor = false;
+            this.b8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b9
+            // 
+            this.b9.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b9.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b9.FlatAppearance.BorderSize = 0;
+            this.b9.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b9.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b9.Location = new System.Drawing.Point(229, 42);
+            this.b9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(100, 100);
+            this.b9.TabIndex = 2;
+            this.b9.UseVisualStyleBackColor = false;
+            this.b9.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b4
+            // 
+            this.b4.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b4.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b4.FlatAppearance.BorderSize = 0;
+            this.b4.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b4.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b4.Location = new System.Drawing.Point(13, 148);
+            this.b4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(100, 100);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = false;
+            this.b4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b5
+            // 
+            this.b5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b5.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b5.FlatAppearance.BorderSize = 0;
+            this.b5.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b5.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b5.Location = new System.Drawing.Point(121, 148);
+            this.b5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(100, 100);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = false;
+            this.b5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b6
+            // 
+            this.b6.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b6.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b6.FlatAppearance.BorderSize = 0;
+            this.b6.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b6.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b6.Location = new System.Drawing.Point(229, 148);
+            this.b6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(100, 100);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = false;
+            this.b6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b1.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b1.FlatAppearance.BorderSize = 0;
+            this.b1.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b1.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b1.Location = new System.Drawing.Point(13, 254);
+            this.b1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 100);
+            this.b1.TabIndex = 6;
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b2.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b2.FlatAppearance.BorderSize = 0;
+            this.b2.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b2.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b2.Location = new System.Drawing.Point(121, 254);
+            this.b2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(100, 100);
+            this.b2.TabIndex = 7;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.b3.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.b3.FlatAppearance.BorderSize = 0;
+            this.b3.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightGray;
+            this.b3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b3.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.b3.Location = new System.Drawing.Point(229, 254);
+            this.b3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(100, 100);
+            this.b3.TabIndex = 8;
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripFile});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(344, 25);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripFile
+            // 
+            this.toolStripFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.toolStripFile.Image = ((System.Drawing.Image)(resources.GetObject("toolStripFile.Image")));
+            this.toolStripFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripFile.Name = "toolStripFile";
+            this.toolStripFile.Size = new System.Drawing.Size(38, 22);
+            this.toolStripFile.Text = "File";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "New Game";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(344, 372);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Font = new System.Drawing.Font("Rubik", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool turnX = true;
+        int turns = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckWin()
+        {
+            bool gameOver = false;
+
+            if ((b1.Text == b2.Text) && (b1.Text == b3.Text) && (!b1.Enabled))
+                gameOver = true; 
+            if ((b4.Text == b5.Text) && (b4.Text == b6.Text) && (!b4.Enabled))
+                gameOver = true; 
+            if ((b7.Text == b8.Text) && (b7.Text == b9.Text) && (!b7.Enabled))
+                gameOver = true;
+
+            if ((b1.Text == b4.Text) && (b1.Text == b7.Text) && (!b1.Enabled))
+                gameOver = true;
+            if ((b2.Text == b5.Text) && (b2.Text == b8.Text) && (!b2.Enabled))
+                gameOver = true;
+            if ((b3.Text == b6.Text) && (b3.Text == b9.Text) && (!b3.Enabled))
+                gameOver = true;
+
+            if ((b1.Text == b5.Text) && (b1.Text == b9.Text) && (!b1.Enabled))
+                gameOver = true;
+            if ((b3.Text == b5.Text) && (b3.Text == b7.Text) && (!b3.Enabled))
+                gameOver = true;
+
+            if (gameOver == true)
+            {
+                String winner;
+                if (turnX)
+                    winner = "O";
+                else
+                    winner = "X";
+                MessageBox.Show(winner + " wins!");
+
+                b1.Enabled = false;
+                b2.Enabled = false;
+                b3.Enabled = false;
+                b4.Enabled = false;
+                b5.Enabled = false;
+                b6.Enabled = false;
+                b7.Enabled = false;
+                b8.Enabled = false;
+                b9.Enabled = false;
+            }
+            else
+            {
+                if (turns == 9)
+                    MessageBox.Show("Draw!");
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button pressed = (Button)sender;
+            if (turnX)
+                pressed.Text = "X";
+            else
+                pressed.Text = "O";
+
+            turns++;
+            turnX = !turnX;
+            pressed.Enabled = false;
+            CheckWin();
+        }            
+
+        private void ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            turnX = true;
+            turns = 0;                     
+
+            b1.Text = "";
+            b1.Enabled = true;
+            b2.Text = "";
+            b2.Enabled = true;
+            b3.Text = "";
+            b3.Enabled = true;
+            b4.Text = "";
+            b4.Enabled = true;
+            b5.Text = "";
+            b5.Enabled = true;
+            b6.Text = "";
+            b6.Enabled = true;
+            b7.Text = "";
+            b7.Enabled = true;
+            b8.Text = "";
+            b8.Enabled = true;
+            b9.Text = "";
+            b9.Enabled = true;
+        }
+    }
+}
